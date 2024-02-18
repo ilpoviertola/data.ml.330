3f3fcabfc5f00272a0dfb544f67ab194f7a2275e
Ilpo Viertola <ilpo.viertola@tuni.fi>
2024-02-18 15:47:50 +0200

    fix: emotion cfg

--- nanoGPT/config/train_emotion.py
+++ nanoGPT/config/train_emotion.py
@@ -1,7 +1,7 @@
 # for classifying emotions
 
 out_dir = "out-emotion"
-eval_interval = 1000  # keep frequent because we'll overfit
+eval_interval = 250  # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10  # don't print too too often
 
@@ -15,7 +15,7 @@ wandb_run_name = "nano-gpt"
 dataset = "emotion"
 gradient_accumulation_steps = 1
 batch_size = 64
-block_size = 240  # context of up to 240 previous characters (max tweet length)
+block_size = 120  # context of up to 240 previous characters (max tweet length)
 
 # baby GPT model :)
 model_type = "gpt4sentimentanalysis"
@@ -25,8 +25,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3  # with baby networks can afford to go a bit higher
-max_iters = 50_000
-lr_decay_iters = 50_000  # make equal to max_iters usually
+max_iters = 5_000
+lr_decay_iters = 5_000  # make equal to max_iters usually
 min_lr = 1e-4  # learning_rate / 10 usually
 beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
 
