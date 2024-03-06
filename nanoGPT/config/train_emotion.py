66bfa1f4a9469109c701e264ca30a56f780ae2a7
Ilpo Viertola <ilpo.viertola@tuni.fi>
2024-03-06 10:38:14 +0200

    fixed hparams

--- nanoGPT/config/train_emotion.py
+++ nanoGPT/config/train_emotion.py
@@ -27,7 +27,7 @@ dropout = 0.2
 learning_rate = 1e-5  # with baby networks can afford to go a bit higher
 max_iters = 5_000
 lr_decay_iters = 5_000  # make equal to max_iters usually
-min_lr = 1e-4  # learning_rate / 10 usually
+min_lr = 1e-6  # learning_rate / 10 usually
 beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100  # not super necessary potentially
